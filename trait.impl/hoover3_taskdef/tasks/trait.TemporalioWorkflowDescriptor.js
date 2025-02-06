e708a5174e2a41f42bda6b65479e7e9b8321c95e
gabriel-v <gabriel-v@users.noreply.github.com>
2025-02-06 02:52:47 +0000

    deploy: 70644afce43646b4d9f8bb062e0ba686ac4127b2

--- trait.impl/hoover3_taskdef/tasks/trait.TemporalioWorkflowDescriptor.js
+++ trait.impl/hoover3_taskdef/tasks/trait.TemporalioWorkflowDescriptor.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["hoover3_filesystem_scanner",[["impl <a class=\"trait\" href=\"hoover3_taskdef/tasks/trait.TemporalioWorkflowDescriptor.html\" title=\"trait hoover3_taskdef::tasks::TemporalioWorkflowDescriptor\">TemporalioWorkflowDescriptor</a> for <a class=\"struct\" href=\"hoover3_filesystem_scanner/struct.fs_scan_datasource_group_workflow.html\" title=\"struct hoover3_filesystem_scanner::fs_scan_datasource_group_workflow\">fs_scan_datasource_group_workflow</a>"],["impl <a class=\"trait\" href=\"hoover3_taskdef/tasks/trait.TemporalioWorkflowDescriptor.html\" title=\"trait hoover3_taskdef::tasks::TemporalioWorkflowDescriptor\">TemporalioWorkflowDescriptor</a> for <a class=\"struct\" href=\"hoover3_filesystem_scanner/struct.fs_scan_datasource_workflow.html\" title=\"struct hoover3_filesystem_scanner::fs_scan_datasource_workflow\">fs_scan_datasource_workflow</a>"]]],["hoover3_taskdef",[]]]);
+    var implementors = Object.fromEntries([["hoover3_filesystem_scanner",[["impl TemporalioWorkflowDescriptor for <a class=\"struct\" href=\"hoover3_filesystem_scanner/struct.fs_scan_datasource_group_workflow.html\" title=\"struct hoover3_filesystem_scanner::fs_scan_datasource_group_workflow\">fs_scan_datasource_group_workflow</a>"],["impl TemporalioWorkflowDescriptor for <a class=\"struct\" href=\"hoover3_filesystem_scanner/struct.fs_scan_datasource_workflow.html\" title=\"struct hoover3_filesystem_scanner::fs_scan_datasource_workflow\">fs_scan_datasource_workflow</a>"]]],["hoover3_taskdef",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[862,23]}
+//{"start":57,"fragment_lengths":[536,23]}
