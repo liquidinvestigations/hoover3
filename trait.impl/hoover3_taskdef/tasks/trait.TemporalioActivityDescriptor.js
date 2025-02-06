58ffd8059e258347ed3b4c3a9d115bbf96841064
gabriel-v <gabriel-v@users.noreply.github.com>
2025-02-06 02:16:48 +0000

    deploy: d6b79165ce01abca4208029282e810bf6776df48

--- trait.impl/hoover3_taskdef/tasks/trait.TemporalioActivityDescriptor.js
+++ trait.impl/hoover3_taskdef/tasks/trait.TemporalioActivityDescriptor.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["hoover3_filesystem_scanner",[["impl <a class=\"trait\" href=\"hoover3_taskdef/tasks/trait.TemporalioActivityDescriptor.html\" title=\"trait hoover3_taskdef::tasks::TemporalioActivityDescriptor\">TemporalioActivityDescriptor</a> for <a class=\"struct\" href=\"hoover3_filesystem_scanner/struct.fs_do_scan_datasource_activity.html\" title=\"struct hoover3_filesystem_scanner::fs_do_scan_datasource_activity\">fs_do_scan_datasource_activity</a>"],["impl <a class=\"trait\" href=\"hoover3_taskdef/tasks/trait.TemporalioActivityDescriptor.html\" title=\"trait hoover3_taskdef::tasks::TemporalioActivityDescriptor\">TemporalioActivityDescriptor</a> for <a class=\"struct\" href=\"hoover3_filesystem_scanner/struct.fs_save_dir_scan_total_result_activity.html\" title=\"struct hoover3_filesystem_scanner::fs_save_dir_scan_total_result_activity\">fs_save_dir_scan_total_result_activity</a>"]]],["hoover3_taskdef",[]]]);
+    var implementors = Object.fromEntries([["hoover3_filesystem_scanner",[["impl TemporalioActivityDescriptor for <a class=\"struct\" href=\"hoover3_filesystem_scanner/struct.fs_do_scan_datasource_activity.html\" title=\"struct hoover3_filesystem_scanner::fs_do_scan_datasource_activity\">fs_do_scan_datasource_activity</a>"],["impl TemporalioActivityDescriptor for <a class=\"struct\" href=\"hoover3_filesystem_scanner/struct.fs_save_dir_scan_total_result_activity.html\" title=\"struct hoover3_filesystem_scanner::fs_save_dir_scan_total_result_activity\">fs_save_dir_scan_total_result_activity</a>"]]],["hoover3_taskdef",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[886,23]}
+//{"start":57,"fragment_lengths":[560,23]}
