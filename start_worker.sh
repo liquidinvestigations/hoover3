5e119ea7f89c9e6aaf02d9c930fd8053cba6ff2c
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2025-02-06 03:25:24 +0200

    don't overwrite cargo dir

--- start_worker.sh
+++ start_worker.sh
@@ -3,9 +3,8 @@ set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 
-export CARGO_TARGET_DIR="target/hoover3_worker"
+# export CARGO_TARGET_DIR="target/hoover3_worker"
 # cargo watch --delay 2 -x run --workdir tasks/filesystem_scanner
 (
-    cd tasks/filesystem_scanner
-    cargo run
+    cargo run --bin worker_filesystem_scanner
 )
